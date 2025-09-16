2807481d0eb53eb1e585467552e0eee5efb034a5
gabriel <g.martin@htlkrems.at>
2025-09-16 10:40:13 +0200

    Add HDR skybox

--- src/Renderer/BlackHoleRenderer.h
+++ src/Renderer/BlackHoleRenderer.h
@@ -5,6 +5,7 @@
 #include "Shader.h"
 #include "Scene.h"
 #include "Camera.h"
+#include "Image.h"
 
 class BlackHoleRenderer {
 public:
@@ -22,10 +23,12 @@ public:
 private:
     void CreateComputeTexture();
     void CreateFullscreenQuad();
+    void LoadSkybox();
     void UpdateUniforms(const std::vector<BlackHole>& blackHoles, const Camera& camera, float time);
 
     std::unique_ptr<Shader> m_computeShader;
     std::unique_ptr<Shader> m_displayShader;
+    std::unique_ptr<Image> m_skyboxTexture;
 
     unsigned int m_computeTexture;
     unsigned int m_quadVAO, m_quadVBO;
--- src/Renderer/Image.h
+++ src/Renderer/Image.h
@@ -1,10 +1,16 @@
 #pragma once
+#include <string>
 
 class Image {
 public:
     unsigned int textureID;
     int width, height;
+
     Image(int width, int height);
     ~Image();
-};
 
+    static Image* LoadHDR(const std::string& filepath);
+
+private:
+    Image(unsigned int textureID, int width, int height);
+};
