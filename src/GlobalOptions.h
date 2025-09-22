6e419fa1ee1a72bf06c04369316b9f830d4913fc
gabriel <g.martin@htlkrems.at>
2025-09-22 13:48:12 +0200

    kerr debug modes

--- src/GlobalOptions.h
+++ src/GlobalOptions.h
@@ -7,8 +7,6 @@ struct GlobalOptions {
     bool kerrDistortionEnabled = true;
     int kerrLutResolution = 64;
     float kerrMaxDistance = 100.0f;
-
-    // Debug: visualize Kerr LUT overlay
     bool kerrDebugLut = false;
     int kerrDebugMode = 0;
 };
