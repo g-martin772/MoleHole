99badb4ec7d3872cd1a9b763c11af003ad846f2d
gabriel <g.martin@htlkrems.at>
2025-10-13 07:54:50 +0200

    fix screenshot height

--- src/Renderer/Screenshot.h
+++ src/Renderer/Screenshot.h
@@ -5,19 +5,11 @@
 
 class Screenshot {
 public:
-    // Nimmt einen Screenshot des aktuellen Viewports auf
     static bool CaptureViewport(int x, int y, int width, int height, const std::string& filename);
-
-    // Nimmt einen Screenshot des gesamten Fensters auf
     static bool CaptureWindow(const std::string& filename);
-
-    // Generiert einen automatischen Dateinamen mit Zeitstempel
     static std::string GenerateTimestampedFilename(const std::string& prefix = "screenshot", const std::string& extension = ".png");
 
 private:
-    // Speichert Pixel-Daten als PNG-Datei
     static bool SavePNG(const std::string& filename, int width, int height, const std::vector<unsigned char>& pixels);
-
-    // Flippt das Bild vertikal (OpenGL-Koordinaten korrigieren)
     static void FlipImageVertically(std::vector<unsigned char>& pixels, int width, int height, int channels);
 };
