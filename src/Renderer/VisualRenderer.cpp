82f8dbe717b454d08cd38cd94f6c85c35e7d3d7a
Leonhard-Neuhold <leonhard.neuhold@outlook.de>
2025-10-21 12:58:48 +0200

    own renderer not working

--- src/Renderer/VisualRenderer.cpp
+++ src/Renderer/VisualRenderer.cpp
@@ -9,11 +9,12 @@
 #include <iostream>
 
 
-
+    
 VisualRenderer::VisualRenderer()
-    : m_quadVAO(0), m_quadVBO(0), m_width(800), m_height(600) { }
+    : m_texture(0), m_quadVAO(0), m_quadVBO(0), m_width(800), m_height(600) { }
 
 VisualRenderer::~VisualRenderer() {
+    if (m_texture) glDeleteTextures(1, &m_texture);
     if (m_quadVAO) glDeleteVertexArrays(1, &m_quadVAO);
     if (m_quadVBO) glDeleteBuffers(1, &m_quadVBO);
 }
@@ -23,25 +24,58 @@ void VisualRenderer::Init(int width, int height) {
     m_height = height;
 
     m_displayShader = std::make_unique<Shader>(
-      "../shaders/black_hole_visual_display.vert",
-      "../shaders/black_hole_visual_display.frag"
+        "../shaders/blackhole_display.vert",
+        "../shaders/blackhole_display.frag"
+    );
+
+    m_computeShader = std::make_unique<Shader>(
+        "../shaders/black_hole_rendering.comp",
+        true
     );
 
     galaxy = loadCubemap("../assets/skybox_nebula_dark");
     colorMap = loadTexture2D("../assets/color_map.png");
-
+    
+    CreateComputeTexture();
     CreateFullscreenQuad();
 }
 
-void VisualRenderer::Resize(int width, int height) {
-    m_width = width;
-    m_height = height;
-}
+void VisualRenderer::Render(std::vector<BlackHole> &black_holes, const Camera &camera, float time) {
 
-void VisualRenderer::Render(const Camera &camera, float time) {
+    SetUniforms(black_holes, camera, time);
+  
+    m_computeShader->Bind();
 
+    glBindImageTexture(0, m_texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+
+    if (galaxy) {
+      glActiveTexture(GL_TEXTURE1);
+      glBindTexture(GL_TEXTURE_3D, galaxy);
+      m_computeShader->SetInt("u_skyboxTexture", 1);
+    }
+
+    if (colorMap) {
+      glActiveTexture(GL_TEXTURE2); 
+      glBindTexture(GL_TEXTURE_3D, colorMap);
+      m_computeShader->SetInt("u_colorMap", 2);
+    }
+
+  
+    unsigned int groupsX = (m_width + 15) / 16;
+    unsigned int groupsY = (m_height + 15) / 16;
+    m_computeShader->Dispatch(groupsX, groupsY, 1);
+
+    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+  
+    m_computeShader->Unbind();
+  
     m_displayShader->Bind();
 
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+    m_displayShader->SetInt("u_raytracedImage", 0);
+  
+    /*
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_CUBE_MAP, galaxy);
     glActiveTexture(GL_TEXTURE1);
@@ -87,11 +121,35 @@ void VisualRenderer::Render(const Camera &camera, float time) {
     m_displayShader->SetFloat("mouseX", 0.0f);
     m_displayShader->SetFloat("mouseY", 0.0f);
 
+    */
+
     glBindVertexArray(m_quadVAO);
     glDrawArrays(GL_TRIANGLES, 0, 6);
     glBindVertexArray(0);
 
     m_displayShader->Unbind();
+}
+
+void VisualRenderer::CreateComputeTexture() {
+    if (m_texture) {
+        glDeleteTextures(1, &m_texture);
+    }
+
+    glGenTextures(1, &m_texture);
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, m_width, m_height, 0, GL_RGBA, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glBindImageTexture(0, m_texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+}
+
+void VisualRenderer::Resize(int width, int height) {
+    m_width = width;
+    m_height = height;
+    CreateComputeTexture();
 }
 
 void VisualRenderer::CreateFullscreenQuad() {
@@ -191,4 +249,25 @@ GLuint VisualRenderer::loadCubemap(const std::string &cubemapDir) {
   glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
   return textureID;
+}
+
+void VisualRenderer::SetUniforms(std::vector<BlackHole>& blackHoles, const Camera& camera, float time) {
+    m_computeShader->Bind();
+
+    int numBlackHoles = std::min(static_cast<int>(blackHoles.size()), 8);
+    m_computeShader->SetInt("u_numBlackHoles", numBlackHoles);
+
+    for (int i = 0; i < numBlackHoles; i++) {
+        m_computeShader->SetVec3("u_blackHolePositions[" + std::to_string(i) + "]", blackHoles[i].position);
+        m_computeShader->SetFloat("u_blackHoleMasses[" + std::to_string(i) + "]", blackHoles[i].mass);
+    }
+
+    m_computeShader->SetFloat("u_time", time);
+    m_computeShader->SetVec3("u_cameraPosition", camera.GetPosition());
+    m_computeShader->SetVec3("u_cameraFront", camera.GetFront());
+    m_computeShader->SetVec3("u_cameraUp", camera.GetUp());
+    m_computeShader->SetVec3("u_cameraRight", glm::normalize(glm::cross(camera.GetFront(), camera.GetUp())));
+    m_computeShader->SetFloat("u_fov", camera.GetFov());
+  
+    m_computeShader->Unbind();
 }
